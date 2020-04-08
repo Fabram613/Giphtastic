c353ba8b600977217c6d7f3ee7009100913e974c
Fabian <49567569+Fabram613@users.noreply.github.com>
2020-04-08 17:01:34 -0500

    add javascript function for buttons

--- javascript/app.js
+++ javascript/app.js
@@ -37,7 +37,37 @@ $(document).ready(function(){
 				//Adds each new button to the HTML element
 				$("#buttonsDiv").append(newButtons);
 			}
-		}
+        }
+        
+        //This calls the function to create the buttons at the beginning of the load page event.
+		createButtons();
+
+		//This function grabs the user input and adds it to the array of buttons
+		$("#submitButton").on("click", function(event) {
+
+			// event.preventDefault() prevents submit button from trying to send a form.
+    		// Using a submit button instead of a regular button allows the user to hit
+    		// "Enter" instead of clicking the button if desired
+    		event.preventDefault();
+
+    		//Create a new variable that holds the user's input
+			var newUserButton = document.forms["inputForm"]["userInput"].value;
+
+			//Add new button to the Array
+			topics.push(newUserButton);
+			
+			//First, we need to empty the div that contains the buttons
+			$("#buttonsDiv").empty();
+			
+			//Create all buttons again
+			createButtons();
+
+			//Testing & Debugging
+			console.log(newUserButton);
+			console.log(topics);
+        });
+        
+    
 
 
            
