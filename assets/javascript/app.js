91316e8dc1f983a14df3c8c8bcecd1104fefb032
Fabian <49567569+Fabram613@users.noreply.github.com>
2020-04-11 15:02:19 -0500

    update js with click event, api url, and ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,10 +29,10 @@ $(document).ready(function(){
             for (var i = 0; i < heroes.length; i++) {
 
                 //Creates a new variable to contain the syntax to create each button
-				var newButtons = $('<button type="button" value="' + topics[i] + '">' + topics[i] + "</button>").addClass("allButtons");
+				var newButtons = $('<button type="button" value="' + heroes[i] + '">' + heroes[i] + "</button>").addClass("allButtons");
 
 				//Adds data property to each new Button
-				newButtons.attr({"data-show": topics[i] });
+				newButtons.attr({"data-show": heroes[i] });
 
 				//Adds each new button to the HTML element
 				$("#buttonsDiv").append(newButtons);
@@ -54,7 +54,7 @@ $(document).ready(function(){
 			var newUserButton = document.forms["inputForm"]["userInput"].value;
 
 			//Add new button to the Array
-			topics.push(newUserButton);
+			heroes.push(newUserButton);
 			
 			//First, we need to empty the div that contains the buttons
 			$("#buttonsDiv").empty();
@@ -64,9 +64,32 @@ $(document).ready(function(){
 
 			//Testing & Debugging
 			console.log(newUserButton);
-			console.log(topics);
+			console.log(heroes);
         });
-        
+		
+		//This function captures the user's choice and runs the AJAX API
+		$(document).on("click", ".allButtons", function() {
+			//Save user's choice variable new value
+			var userChoice = $(this).data("show");
+
+			//Converts user's choice variable to an encoded string for URL purposes
+			var userChoiceEncoded = encodeURI(userChoice);
+
+			//Create a new variable fo the GIPHY API query URL search
+			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userChoiceEncoded + "&limit=9&api_key=Tvo82nkMez1DGbcZ8FSVqojBMydBpDzi";
+
+			//AJAX call
+			$.ajax({
+				url: queryURL,
+				method: "GET"
+			}).done(function(response) {
+
+				//Testing
+				console.log(response);
+
+			});
+
+		});
     
 
 
