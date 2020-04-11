44dc14341f8a98244d51b0228b1b7a75b6d971b8
Fabian <49567569+Fabram613@users.noreply.github.com>
2020-04-11 15:33:02 -0500

    update js with variables for populating rows

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -87,9 +87,64 @@ $(document).ready(function(){
 				//Testing
 				console.log(response);
 
+				//Creating a new variable to contain the syntax for get the first row of the table
+				var firstRowTds = $("table").children().eq(1).children("tr").eq(0).children("td");
+				//Sets the inner content of each td in the first row
+				firstRowTds.eq(0).html('<img src=' + response.data[0].images.fixed_height_still.url + '" alt="' + response.data[0].title + '">"');
+				firstRowTds.eq(1).html('<img src=' + response.data[1].images.fixed_height_still.url + '" alt="' + response.data[1].title + '">"');
+				firstRowTds.eq(2).html('<img src=' + response.data[2].images.fixed_height_still.url + '" alt="' + response.data[2].title + '">"');
+
+				//Testing 
+				console.log(response);
+
+				//Creating a new variable to contain the syntax to get the second row of the table
+				var secondRowTds = $("table").children().eq(1).children('tr').eq(1).children('td');
+				//Sets the inner content of each td in the second row
+				secondRowTds.eq(0).html("This gif is rated:" + response.data[0].rating);
+				secondRowTds.eq(1).html("This gif is rated:" + response.data[1].rating);
+				secondRowTds.eq(2).html("This gif is rated:" + response.data[2].rating);
+
+				//Creating a new variable to contain the syntax to get the third row of the table
+				var thirdRowTds = $("table").children().eq(1).children('tr').eq(2).children('td');
+				//Sets the inner content of each td in the third row
+				thirdRowTds.eq(0).html('<img src=' + response.data[3].images.fixed_height_still.url + '" alt="' + response.data[3].title + '">"');
+				thirdRowTds.eq(1).html('<img src=' + response.data[4].images.fixed_height_still.url + '" alt="' + response.data[4].title + '">"');
+				thirdRowTds.eq(2).html('<img src=' + response.data[5].images.fixed_height_still.url + '" alt="' + response.data[5].title + '">"');
+
+				//Creating a new variable to contain the syntax to get the fourth row of the table
+				var secondRowTds = $("table").children().eq(1).children('tr').eq(4).children('td');
+				//Sets the inner content of each td in the fourth row
+				fourthRowTds.eq(0).html("This gif is rated:" + response.data[3].rating);
+				fourthRowTds.eq(1).html("This gif is rated:" + response.data[4].rating);
+				fourthRowTds.eq(2).html("This gif is rated:" + response.data[5].rating);
+
+				//Creating a new variable to contain the syntax to get the fifth row of the table
+				var fifthRowTds = $("table").children().eq(1).children('tr').eq(2).children('td');
+				//Sets the inner content of each td in the fifth row
+				fifthRowTds.eq(0).html('<img src=' + response.data[6].images.fixed_height_still.url + '" alt="' + response.data[6].title + '">"');
+				fifthRowTds.eq(1).html('<img src=' + response.data[7].images.fixed_height_still.url + '" alt="' + response.data[7].title + '">"');
+				fifthRowTds.eq(2).html('<img src=' + response.data[8].images.fixed_height_still.url + '" alt="' + response.data[8].title + '">"');
+
+				//Creating a new variable to contain the syntax to get the sixth row of the table
+				var sixthRowTds = $("table").children().eq(1).children('tr').eq(4).children('td');
+				//Sets the inner content of each td in the sixth row
+				sixthRowTds.eq(0).html("This gif is rated:" + response.data[6].rating);
+				sixthRowTds.eq(1).html("This gif is rated:" + response.data[7].rating);
+				sixthRowTds.eq(2).html("This gif is rated:" + response.data[8].rating);
+
+				//Applies a calls to all of the img elements
+				$('img').addClass("allGiphyImages");
+
+				//Testing
+				console.log(userChoice);
+				console.log(userChoiceEncoded);
+				console.log(queryURL);
+
 			});
 
 		});
+
+	//	
     
 
 
